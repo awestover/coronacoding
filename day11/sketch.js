eb4aea9b2b4d7d800803719c97de27a799b8c950
Alek Westover <alek.westover@gmail.com>
2020-04-04 14:37:43 -0400

    hey madeline heres some code

--- day11/sketch.js
+++ day11/sketch.js
@@ -1,10 +1,17 @@
 
+let avatarImg;
 
 function setup(){
   createCanvas(500,500);
+  avatarImg = loadImage("avatar.png");
 }
 
 function draw(){
   background("#00ffff");
+  image(avatarImg, 10,10, 75, 100);
+  if(keyIsDown(65)){// "a"
+    alert("65");
+  }
 }
+
 
